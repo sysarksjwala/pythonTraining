d814d6f9ab03d7dafddc9779979d648f82c11cc5
sysarksjwala <ujjwala2136@gmail.com>
2018-04-09 15:00:33 +0530

    string count

---
+++ Stringcount.py
@@ -0,0 +1,14 @@
+print("A program for Average of two numbers")
+print()
+print("Average")
+string=raw_input("Enter string:")
+count1=0
+count2=0
+for i in string:
+      if(i.isdigit()):
+            count1=count1+1
+      count2=count2+1
+print("The number of digits is:")
+print(count1)
+print("The number of characters is:")
+print(count2)
