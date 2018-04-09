9e55b0b5ff2e1650fd38605687f3eb1109c386cb
sysarksjwala <ujjwala2136@gmail.com>
2018-04-09 14:59:24 +0530

    string replacement

---
+++ Rstring.py
@@ -0,0 +1,10 @@
+print("A program to Replacement of string")
+print()
+print("String Replacement")
+# initialize a string
+string=input("Enter string:")
+#Replacing string 'a','A' with '$','@'
+string=string.replace('a','$')
+string=string.replace('A','@')
+print("Modified string:")
+print(string)
