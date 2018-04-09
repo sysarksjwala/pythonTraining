50f7b65e3b3104ce7fb014df1c5902c7fa2524aa
sysarksjwala <ujjwala2136@gmail.com>
2018-04-09 14:51:27 +0530

    Length of string

---
+++ Lstring.py
@@ -0,0 +1,10 @@
+print("A program to find length of string without library functions")
+print()
+print("String Length") 
+# Declaring a string
+string=input("Enter string:")
+count=0
+for i in string:
+      count=count+1
+print("Length of the string is:")
+print(count)
