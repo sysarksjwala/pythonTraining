82aef5e0ff3d5057973914ad38b77b137e9251d1
sysarksjwala <ujjwala2136@gmail.com>
2018-04-09 14:41:54 +0530

    Prints the given statements

--- ex1.py
+++ ex1.py
@@ -1,9 +1,18 @@
+# prints statements
 print ("Hello world")
 print ("This is atom")
-print("Hello World!")
-print("Hello Again")
 print("I like typing this.")
-print("This is fun.")
 print('Yay! Printing.')
 print("I'd much rather you 'not'.")
 print('I "said" do not touch this.')
+
+#print blank line
+print(5 * "\n")
+
+#print objects in same line
+x,y=1,'jwala'
+print(x),;print(y)
+
+#Formatted strings
+print("pi=%s" % "3.14159")
+
