103f77886c163f60558afefe8bde2c5dda482a5e
sysarksjwala <ujjwala2136@gmail.com>
2018-04-09 12:42:13 +0530

    Basic Mathematical operations

---
+++ operations.py
@@ -0,0 +1,40 @@
+print("A program for simple mathematical operations dynamically")
+print()
+print("Addition operation")
+print()
+# Initializing input numbers
+X = input('Enter first number:')
+Y = input('Enter second number:')
+# add two numbers
+sum = float(X) + float(Y)
+#Addition
+print("The addition is",sum)
+print()
+print("Substraction operation")
+print()
+# Initializing input numbers
+X = input('Enter first number:')
+Y = input('Enter second number:')
+# substract two numbers
+Substraction = float(X) - float(Y)
+#substraction
+print("The substraction is",Substraction)
+print()
+print("Division operation")
+print()
+# Initializing input numbers
+X = input('Enter first number:')
+Y = input('Enter second number:')
+# divide two numbers
+Division = float(X) / float(Y)
+#Division
+print("The division is",Division)
+print()
+print("squareroot operation")
+print()
+# Initializing input number
+X = input('Enter first number:')
+# squareroot
+squareroot = float(X) ** 0.5
+print("The squareroot is",squareroot)
+
