b85b3b2a586db0d4635224a2117b4a70279c60ba
sysarksjwala <ujjwala2136@gmail.com>
2018-04-09 12:45:49 +0530

    Calculate the Average of Given Numbers

---
+++ Average.py
@@ -0,0 +1,13 @@
+print("A program for Average of two numbers")
+print()
+print("Average") 
+#initializing variable 
+X=int(input("Enter the number of elements to be inserted: "))
+#initializing an array
+a=[]
+# for loop for range 
+for i in range(0,X):
+   num=int(input("Enter element: "))
+   a.append(num)
+avg=sum(a)/X
+print("Average of elements in the list",round(avg,2))
