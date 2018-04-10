6b230d1e8eef91139f881db5adcb609879c987d8
sysarksjwala <ujjwala2136@gmail.com>
2018-04-10 16:16:37 +0530

    Find Square root using method

---
+++ Functionspractice/SquareRoot.py
@@ -0,0 +1,15 @@
+print ("program to find squareroot using Functions" )
+print()
+# sqrt() method 
+
+# import the math module 
+import math 
+
+# print the square root of 0 
+print(math.sqrt(10)) 
+
+# print the square root of 4
+print(math.sqrt(486)) 
+
+# print the square root of 3.5
+print(math.sqrt(4.567)) 
