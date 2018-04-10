7e17fe4bc847934264841e6af98c89294c1c1f6e
sysarksjwala <ujjwala2136@gmail.com>
2018-04-10 15:26:28 +0530

    Functionspractice/AdditionFunc.py

---
+++ Functionspractice/AdditionFunc.py
@@ -0,0 +1,10 @@
+print("A program Add Lists using Functions")
+print()
+print("Addition List Function")
+# initializing a list 
+Course = [3000,4000,5000];
+CourseFee=sum(Course)
+print(CourseFee)
+
+
+
---
+++ Functionspractice/PrintFunc.py
@@ -0,0 +1,9 @@
+print("A program to print a statement using Function")
+print()
+# print itself a function
+print("Print Function")
+# initializing a variable
+a=10
+print("The value of a is :", a)
+b=a
+print(" a and b values are equal:", b)
