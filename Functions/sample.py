309079f82a24a70348be877508f3308507d3ea48
sysarksjwala <ujjwala2136@gmail.com>
2018-04-10 16:44:21 +0530

    Sample program

--- Functions/sample.py
+++ Functions/sample.py
@@ -1,3 +1,4 @@
+#This is a sample program
 def function_that_prints():
     print("I printed")
 
@@ -9,3 +10,4 @@ f2 = function_that_returns()
 print ("Now let us see what the values of f1 and f2 are")
 print (f1)
 print (f2)
+
