c115ece58daf7c8a27c42afa9e106316ce50befc
sysarksjwala <ujjwala2136@gmail.com>
2018-04-12 13:59:28 +0530

    Factorial for given number

---
+++ Functions/Factorial.py
@@ -0,0 +1,15 @@
+print("Program for Factorial of Given numbers")
+x = int(input("Enter number:"))
+fact = 1
+if x == 0:
+	print(" The factorial of 0 is always 1 ")
+
+elif x > 0:
+	for i in range(1,x+1):
+		fact=fact * i
+	print("The factorial of",x,"is",fact)
+		
+else:
+	print(" No factorial for negative numbers")
+		
+
